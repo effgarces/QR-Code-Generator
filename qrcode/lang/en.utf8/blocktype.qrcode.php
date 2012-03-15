35c6fae574f402247e919f4690dbcf7d71d05291
Emanuel Garcês <emanuel.garces@live.madeira-edu.pt>
2012-03-15 15:49:46 +0000

    QR Code Generator block for Mahara 1.4.x

---
+++ qrcode/lang/en.utf8/blocktype.qrcode.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ * Mahara: Electronic portfolio, weblog, resume builder and social networking
+ * Copyright (C) 2006-2009 Catalyst IT Ltd and others; see:
+ *                         http://wiki.mahara.org/Contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @package    mahara
+ * @subpackage blocktype-qrcode
+ * @author     Emanuel Garcês
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL
+ * @copyright  (C) 2011 Emanuel Garcês
+ *
+ */
+
+defined('INTERNAL') || die();
+
+$string['title'] = 'QR Code Generator';
+$string['description'] = 'Generate QR codes';
+
+$string['previewonly'] = 'This is static preview preview of your QRCode';
+
+$string['showtitle'] = 'Show Block Title?';
+
+$string['align'] = 'Align';
+$string['alignleft'] = 'Left';
+$string['aligncenter'] = 'Center';
+$string['alignright'] = 'Right';
+
+$string['size'] = 'Size';
+$string['size_small'] = 'Small';
+$string['size_medium'] = 'Medium';
+$string['size_big'] = 'Big';
+$string['size_custom'] = 'Custom size';
+
+$string['height'] = 'Height';
+$string['width'] = 'Width';
+
+$string['type'] = 'Type';
+$string['type_profile'] = 'Profile';
+$string['type_current_page'] = 'Current Page';
+$string['type_page'] = 'Page';
+$string['type_vcard'] = 'vCard';
+$string['type_mecard'] = 'meCard';
+$string['type_email'] = 'Email Address';
+$string['type_emailmessage'] = 'Email Message';
+$string['type_phone'] = 'Phone number';
+$string['type_smsmessage'] = 'Message';
+$string['type_sms'] = 'SMS message';
+$string['type_text'] = 'Text';
+$string['type_url'] = 'Website URL';
+
+$string['website_url'] = 'Website URL';
+$string['email'] = 'Email';
+$string['subject'] = 'Subject';
+$string['phone_number'] = 'Phone number';
+$string['page'] = 'Page';
+
+$string['first_name'] = 'First name';
+$string['last_name'] = 'Last name';
+$string['address'] = 'Address';
+
+$string['missing_name'] = 'Please fill the First Name field and/or Last Name';
+$string['missing_email'] = 'Please fill the email field';
+$string['missing_emailmessage'] = 'Please fill the email and text fields';
+$string['missing_phone'] = 'Please fill the phone number';
+$string['missing_sms'] = 'Please fill the phone number and sms fields';
+$string['missing_text'] = 'Please fill the text field';
+$string['missing_website_url'] = 'Please fill the website url field';
+?>
