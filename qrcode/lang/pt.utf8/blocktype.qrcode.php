3035a77ebed076d8bda4c05b48b1d080fb68f377
Emanuel Garcês <emanuel.garces@live.madeira-edu.pt>
2012-03-20 11:53:18 +0000

    Added Slovenian translation. Contributed by: Gregor Anželj

---
+++ qrcode/lang/pt.utf8/blocktype.qrcode.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * Mahara: Electronic portfolio, weblog, resume builder and social networking
+ * Copyright (C) 2006-2009 Catalyst IT Ltd and others; see:
+ *                         http://wiki.mahara.org/Contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @package    mahara
+ * @subpackage blocktype-qrcode
+ * @author     Emanuel Garcês
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL
+ * @copyright  (C) 2011 Emanuel Garcês
+ *
+ */
+
+defined('INTERNAL') || die();
+
+$string['title'] = 'Gerador de códigos QR';
+$string['description'] = 'Gera códigos QR';
+
+$string['previewonly'] = 'Esta é uma pré-visualização estática do do seu código QR';
+
+$string['showtitle'] = 'Mostrar título do bloco?';
+
+$string['align'] = 'Alinhar';
+$string['alignleft'] = 'Esquerda';
+$string['aligncenter'] = 'Centro';
+$string['alignright'] = 'Direita';
+
+$string['size'] = 'Tamanho';
+$string['size_small'] = 'Pequeno';
+$string['size_medium'] = 'Médio';
+$string['size_big'] = 'Grande';
+$string['size_custom'] = 'Tamanho personalizado';
+
+$string['height'] = 'Altura';
+$string['width'] = 'Largura';
+
+$string['type'] = 'Tipo';
+$string['type_profile'] = 'Perfil';
+$string['type_current_page'] = 'Página atual';
+$string['type_page'] = 'Página';
+$string['type_vcard'] = 'vCard';
+$string['type_mecard'] = 'meCard';
+$string['type_email'] = 'Endereço de email';
+$string['type_emailmessage'] = 'Mensagem de email';
+$string['type_phone'] = 'N.º de telefone';
+$string['type_sms'] = 'Mensagem SMS';
+$string['type_text'] = 'Texto';
+$string['type_url'] = 'Página web';
+
+$string['website_url'] = 'Endereço da página web';
+$string['email'] = 'Email';
+$string['subject'] = 'Assunto';
+$string['phone_number'] = 'N.º de telefone';
+$string['page'] = 'Página';
+
+$string['first_name'] = 'Primeiro nome';
+$string['last_name'] = 'Último nome';
+$string['address'] = 'Endereço';
+
+$string['missing_name'] = 'Por favor preencha o campo "Primeiro nome" e/ou "Último nome"';
+$string['missing_email'] = 'Por favor preencha o campo "Email"';
+$string['missing_emailmessage'] = 'Por favor preencha o campos "Email" e "Texto"';
+$string['missing_phone'] = 'Por favor preencha o campo "N.º de telefone"';
+$string['missing_sms'] = 'Por favor preencha os campos "N.º de telefone" e "Mensagem SMS"';
+$string['missing_text'] = 'Por favor preencha o campo "Texto"';
+$string['missing_website_url'] = 'Por favor preencha o campo "Endereço da página web"';
+?>
---
+++ qrcode/lang/sl.utf8/blocktype.qrcode.php
@@ -0,0 +1,84 @@
+﻿<?php
+/**
+ * Mahara: Electronic portfolio, weblog, resume builder and social networking
+ * Copyright (C) 2006-2009 Catalyst IT Ltd and others; see:
+ *                         http://wiki.mahara.org/Contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @package    mahara
+ * @subpackage blocktype-qrcode
+ * @author     Emanuel Garcês
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL
+ * @copyright  (C) 2011 Emanuel Garcês
+ *
+ * @translator  Gregor Anželj, gregor.anzelj@gmail.com
+ *
+ */
+
+defined('INTERNAL') || die();
+
+$string['title'] = 'Generator QR kod';
+$string['description'] = 'Ustvarite QR kodo';
+
+$string['previewonly'] = 'To je statični predogled QR kode';
+
+$string['showtitle'] = 'Prikažem naslov bloka?';
+
+$string['align'] = 'Poravnava';
+$string['alignleft'] = 'Levo';
+$string['aligncenter'] = 'Sredinsko';
+$string['alignright'] = 'Desno';
+
+$string['size'] = 'Velikost';
+$string['size_small'] = 'Majhno';
+$string['size_medium'] = 'Srednje';
+$string['size_big'] = 'Veliko';
+$string['size_custom'] = 'Lastna velikost';
+
+$string['height'] = 'Višina';
+$string['width'] = 'Širina';
+
+$string['type'] = 'Vrsta';
+$string['type_profile'] = 'Profil';
+$string['type_current_page'] = 'Trenutni pogled';
+$string['type_page'] = 'Pogled';
+$string['type_vcard'] = 'vCard';
+$string['type_mecard'] = 'meCard';
+$string['type_email'] = 'Epoštni naslov';
+$string['type_emailmessage'] = 'Epoštno sporočilo';
+$string['type_phone'] = 'Telefonska številka';
+$string['type_smsmessage'] = 'Sporočilo';
+$string['type_sms'] = 'SMS sporočilo';
+$string['type_text'] = 'Besedilo';
+$string['type_url'] = 'URL naslov';
+
+$string['website_url'] = 'URL naslov';
+$string['email'] = 'Epošta';
+$string['subject'] = 'Zadeva';
+$string['phone_number'] = 'Telefonska številka';
+$string['page'] = 'Stran';
+
+$string['first_name'] = 'Ime';
+$string['last_name'] = 'Priimek';
+$string['address'] = 'Naslov';
+
+$string['missing_name'] = 'Prosimo, izpolnite polje ime in/ali priimek';
+$string['missing_email'] = 'Prosimo, izpolnite polje epošta';
+$string['missing_emailmessage'] = 'Prosimo, izpolnite polji epošta in besedilo';
+$string['missing_phone'] = 'Prosimo, izpolnite polje telefonska številka';
+$string['missing_sms'] = 'Prosimo, izpolnite polji telefonska številka in SMS sporočilo';
+$string['missing_text'] = 'Prosimo, izpolnite polje besedilo';
+$string['missing_website_url'] = 'Prosimo, izpolnite polje spletna stran';
+?>
