287b8ceb8c5fda0fff0368a477b219ec76e1f20c
Emanuel Garcês <effgarces@gmail.com>
2020-10-29 13:27:39 +0000

    Portuguese translation

---
+++ lang/pt.utf8/blocktype.qrcode.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Mahara: Electronic portfolio, weblog, resume builder and social networking
+ * Copyright (C) 2006-2009 Catalyst IT Ltd and others; see:
+ *                         http://wiki.mahara.org/Contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @package    mahara
+ * @subpackage blocktype-qrcode
+ * @author     Emanuel Garc�s
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL
+ * @copyright  (C) 2011 Emanuel Garcês
+ *
+ */
+
+defined('INTERNAL') || die();
+
+$string['title'] = 'Código QR';
+$string['description'] = 'Exibir código QR para endereço da página';
+
+$string['align'] = 'Alinhar';
+$string['alignleft'] = 'Esquerda';
+$string['aligncenter'] = 'Centro';
+$string['alignright'] = 'Direita';
+
+$string['size'] = 'Tamanho';
+$string['size_small'] = 'Pequeno';
+$string['size_medium'] = 'Médio';
+$string['size_big'] = 'Grande';
+$string['size_custom'] = 'Tamanho personalizado';
+
+$string['width'] = 'Comprimento (em pixels)';
